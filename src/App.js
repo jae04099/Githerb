66478009b610990d6678069af3e725b9c6c66fe8
jeongbbn <kkaatteelyn@gmail.com>
2021-06-25 10:10:30 +0900

    make Exp graph

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import TimeLineList from './components/Overview/TimeLineLists/TimeLineList';
+import Exp from './components/Overview/Exp/Exp';
 import { PlantInfoProvider } from './contexts/plant'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <PlantInfoProvider>
       <div className="App">
         <TimeLineList />
+        {/* <Exp style={{ width: "500px", height: "300px" }} /> */}
       </div>
     </PlantInfoProvider>
   );
