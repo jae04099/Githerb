30d2e1740a28c2bc750eebfdc6808bf84018baf9
Gyuri Lee <jae04099@catholic.ac.kr>
2021-06-27 00:21:18 +0900

    calendar component

--- src/components/Overview/Calendar/Calendar.js
+++ src/components/Overview/Calendar/Calendar.js
@@ -1,20 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState, useContext, useEffect } from 'react'
 import { isSameDay } from 'date-fns'
 import ko from 'date-fns/locale/ko'
 import { Calendar } from 'react-nice-dates'
+import { PlantInfoContext } from '../../../contexts/plant'
 import 'react-nice-dates/build/style.css'
 import './Calendar.css'
-// Very rough implementation of multiple date selection
+
+
 function CalendarExample() {
-  const [selectedDates, setSelectedDates] = useState([])
+  const [plantInfo, setPlantInfo] = useContext(PlantInfoContext)
+  const [comDateArr, setComDateArr] = useState([])
+
   const modifiers = {
-    selected: date => selectedDates.some(selectedDate => isSameDay(selectedDate, date))
+    selected: date => comDateArr.some(selectedDate => 
+      isSameDay(selectedDate, date)
+      )
   }
-  const handleDayClick = date => {
-    setSelectedDates([...selectedDates, date])
+
+  // const handleDayClick = date => {
+  //   const testDate = date.toString().substr(4,11)
+  //   console.log(Date.parse(comDateArr[0]))
+  //   console.log(comDateArr[0])
+  //   console.log(new Date(1617321600000))
+  //   moment(testDate, "MMM DD YYYY").format("YYYY-MM-DD")
+  //   //Tue Jun 08 2021 00:00:00 GMT+0900 (대한민국 표준시)
+  //   setSelectedDates([...selectedDates, date])
+  // }
+  
+  useEffect(() => {
+    setComDateArr(getComDate(plantInfo.commit))
+  }, [])
+
+  
+  function getComDate(comDate) {
+    const comDateArr = []
+    for (let i = 0; i < comDate.length; i++) {
+      const date = Date.parse(comDate[i].date.substr(0, 10))
+      const finDate = new Date(date)
+      comDateArr.push(finDate)
+    }
+    return comDateArr;
   }
+  
   return (
-    <Calendar onDayClick={handleDayClick} modifiers={modifiers} locale={ko} />
+    <Calendar modifiers={modifiers} locale={ko} />
   )
 }
 
