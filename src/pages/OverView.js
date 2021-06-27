736926bf30c8a6f2448bcc13511d6ae32c85e515
jeongbbn <kkaatteelyn@gmail.com>
2021-06-27 17:38:19 +0900

    create router

---
+++ src/pages/OverView.js
@@ -0,0 +1,18 @@
+import TimeLineList from '../components/Overview/TimeLineLists/TimeLineList';
+import Exp from '../components/Overview/Exp/Exp';
+import DayCommit from '../components/Overview/DayCommit/DayCommit';
+import LangGraph from '../components/Graph/LanguageGraph/LanguageGraph'
+import NextLevel from '../components/Overview/NextLevel/NextLevel'
+import Calendar from '../components/Overview/Calendar/Calendar'
+import RepoDesc from '../components/Overview/RepoDescription/RepoDescription'
+import PlantProfile from '../components/Overview/PlantProfile/PlantProfile'
+
+import React from 'react'
+
+export default function Overview() {
+  return (
+    <div>
+
+    </div>
+  )
+}
